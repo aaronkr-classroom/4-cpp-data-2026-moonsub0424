01ea71db071dc6a8db2becf27e7c373806d43985
ththfql <131600659+moonsub0424@users.noreply.github.com>
2026-03-25 17:22:40 +0900

    week 4 class

---
+++ week4/grade.h
@@ -0,0 +1,13 @@
+//grade.h
+
+#ifndef GUARD_grade_h
+#define GUARD_grade_info_h
+
+#include <vector>
+#include "Student_info.h"
+
+double grade(double, double, const vector<double>&);
+double grade(double, double, double);
+double grade(const Student_info&);
+
+#endif
---
+++ week4/new_main.cpp
@@ -0,0 +1,64 @@
+//new_main.cpp
+// 중간고사, 기말고사, 그리고 과제 점수 몇 개를 받고
+// 마지막 평균 결과 점수를 계산하기.
+#include <algorithm> // sort()
+#include <ios>
+#include <iostream>
+#include <iomanip> //setprecision()
+#include <string>
+#include <vector>
+#include <stdexcept> // istream& 위해
+#include "Student_info.h"
+#include "grade.h"
+#include "median.h"
+
+using namespace std;
+
+typedef vector<double>::size_type vec_sz;
+typedef vector<Student_info>::size_type si_sz;
+
+
+int main(void) {
+	// 새로운 구조 자료형 선언
+	vector<Student_info> students;
+	Student_info record;
+	string::size_type maxlen = 0;
+
+	// 프로그램 설명하기
+	cout << "For every student, enter: " << endl <<
+		"Name Midterm Final Hw1 Hw2..." << endl;
+
+	while (read(cin, record)) {
+		maxlen = max(maxlen, record.name.size());
+		students.push_back(record);
+	}
+
+	// 학생 정보를 알파벳순으로 정렬
+	sort(students.begin(), students.end(),compare);
+
+	// 모든 학생의 결과를 출력
+	for (si_sz i = 0; i != students.size(); ++i) {
+		// 이름과 오른쪽 공백을 포함하여 maxlen + 1개의 문자를 출력
+		cout << students[i].name <<
+			string(maxlen + 1 - students[i].name.size(), ' ');
+
+		//최종 점수를 계산하여 출력
+		try {
+			double final_grade = grade(students[i]); // NEW 계산하는 함수 grade()
+			streamsize prec = cout.precision();
+			cout << setprecision(3) << final_grade 
+				<< setprecision(prec) << endl;
+		}
+		catch (domain_error e) {
+			cout << e.what();
+		}
+		cout << endl;
+	}
+	
+
+
+
+	return 0;
+}
+
+
