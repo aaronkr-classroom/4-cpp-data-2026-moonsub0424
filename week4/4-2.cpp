9e9bea0ab9eb3e8f87acbfb941ed6daf8be491b3
moonsub <131600659+moonsub0424@users.noreply.github.com>
2026-03-28 13:33:34 +0900

    week4 HomeWork

---
+++ week4/4-2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <ios>
+#include <iomanip>
+
+using namespace std;
+
+int main(void) {
+
+	for (int i = 1; i <= 100; i++) {
+		cout << setw(6) << i << setw(10) << i * i << endl;
+	}
+
+	return 0;
+}
---
+++ week4/4-3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <ios>
+#include <iomanip>
+
+using namespace std;
+
+int main(void) {
+
+	int a, b;
+
+	cout << "변수를 입력하세요: ";
+	cin >> a >> b;
+
+	for (int i = 1; i <= 1000; i++) {
+		cout << setw(a) << i << setw(b) << i * i << endl;
+	}
+
+	return 0;
+}
